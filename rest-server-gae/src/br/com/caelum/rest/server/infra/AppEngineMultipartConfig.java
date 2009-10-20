3d55d2eb4d06559ea8bc193bcd8689761cda75b3
Guilherme Silveira <guilherme@guilherme-silveiras-macbook-pro.local>
2009-10-20 16:38:28 -0200

    rest server on google app engine

---
+++ rest-server-gae/src/br/com/caelum/rest/server/infra/AppEngineMultipartConfig.java
@@ -0,0 +1,21 @@
+package br.com.caelum.rest.server.infra;
+
+import java.io.File;
+
+import br.com.caelum.vraptor.interceptor.multipart.MultipartConfig;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
+@ApplicationScoped
+public class AppEngineMultipartConfig implements MultipartConfig {
+
+	public File getDirectory() {
+		return null;
+	}
+
+	public long getSizeLimit() {
+		return Integer.MAX_VALUE;
+	}
+
+}
