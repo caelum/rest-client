da58f0691842206a9fdace95a2586d9c62a2e9d2
guilherme silveira <guilherme.silveira@caelum.com.br>
2009-10-22 15:28:21 -0200

    read corectly the response

--- rest-client-gae/src/br/com/caelum/rest/client/http/Response.java
+++ rest-client-gae/src/br/com/caelum/rest/client/http/Response.java
@@ -1,6 +1,9 @@
 package br.com.caelum.rest.client.http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.Map;
@@ -8,12 +11,23 @@ import java.util.Map;
 public class Response {
 
 	private int code;
-	private String content;
+	private String content = "";
 	private Map<String, List<String>> headers;
 
 	public Response(HttpURLConnection connection) throws IOException {
 		this.code = connection.getResponseCode();
-		this.content = connection.getContent().toString();
+		InputStream stream =  (InputStream) connection.getContent();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		while(true) {
+			String partial = reader.readLine();
+			if(partial==null){
+				break;
+			}
+			if(content.length()!=0) {
+				content += "\n";
+			}
+			content += partial;
+		}
 		this.headers = connection.getHeaderFields();
 	}
 
